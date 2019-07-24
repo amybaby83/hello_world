9c8e9396eb680c895a6da35e93fb410150b597ae
Amy <39568291+amybaby83@users.noreply.github.com>
2019-07-24 12:29:54 -0400

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+//Amy Blain
+//COP2000.C53
+//Basic Hello Program
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, my name is Amy"  <<endl  <<endl  <<endl;
+	return 0;
+}
